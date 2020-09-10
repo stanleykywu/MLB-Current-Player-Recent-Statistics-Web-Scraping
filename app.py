4a6621ab5005eb9d2655ea798637cf5f90705ce1
stanleykywu <stanleykywu@gmail.com>
2020-09-10 14:48:16 -0400

    Build simple ability to choose stats to display

--- app.py
+++ app.py
@@ -3,15 +3,57 @@ import pandas as pd
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+pitching_data = pd.read_csv('recent_mlb_pitcher_stats.csv', encoding = "ISO-8859-1")
+pitching_data.columns = pitching_data.columns.str.replace(' ', '')
+
+hitting_data = pd.read_csv('recent_mlb_hitter_stats.csv', encoding = "ISO-8859-1")
+hitting_data.columns = hitting_data.columns.str.replace(' ', '')
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         if request.form['submit_button'] == 'Pitching Statistics':
-            data = pd.read_csv('stat_scraper/generated_stats/recent_mlb_pitching_stats.csv')
-            return render_template('index.html', data=data.to_html())
+            return render_template('index.html', current_selection='You have selected pitching statistics', options=['Pitching Last 7 Games', 'Pitching Last 15 Games', 'Pitching Last 30 Games'])
+
         elif request.form['submit_button'] == 'Hitting Statistics':
-            data = pd.read_csv('stat_scraper/generated_stats/recent_mlb_hitting_stats.csv')
+            return render_template('index.html', current_selection='You have selected hitting statistics',  options=['Hitting Last 7 Games', 'Hitting Last 15 Games', 'Hitting Last 30 Games'])
+
+        elif 'Last 7 Games' in request.form['submit_button']:
+            if 'Pitching' in request.form['submit_button']:
+                data = pitching_data[pitching_data['Duration'] == ' Last 7 Games']
+                data = data.sort_values('ERA')
+            elif 'Hitting' in request.form['submit_button']:
+                data = hitting_data[hitting_data['Duration'] == ' Last 7 Games']
+                data = data.sort_values('AVG', ascending=False)
+            else:
+                return 'There was an issue determing the type of statistic to filter'
+
             return render_template('index.html', data=data.to_html())
+
+        elif 'Last 15 Games' in request.form['submit_button']:
+            if 'Pitching' in request.form['submit_button']:
+                data = pitching_data[pitching_data['Duration'] == ' Last 15 Games']
+                data = data.sort_values('ERA')
+            elif 'Hitting' in request.form['submit_button']:
+                data = hitting_data[hitting_data['Duration'] == ' Last 15 Games']
+                data = data.sort_values('AVG', ascending=False)
+            else:
+                return 'There was an issue determing the type of statistic to filter'
+
+            return render_template('index.html', data=data.to_html())
+
+        elif 'Last 30 Games' in request.form['submit_button']:
+            if 'Pitching' in request.form['submit_button']:
+                data = pitching_data[pitching_data['Duration'] == ' Last 30 Games']
+                data = data.sort_values('ERA')
+            elif 'Hitting' in request.form['submit_button']:
+                data = hitting_data[hitting_data['Duration'] == ' Last 30 Games']
+                data = data.sort_values('AVG', ascending=False)
+            else:
+                return 'There was an issue determing the type of statistic to filter'
+
+            return render_template('index.html', data=data.to_html())
+
         else:
             return 'There was an issue selecting the type of statistic'
     return render_template('index.html')
